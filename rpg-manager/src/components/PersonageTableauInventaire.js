759c7b0232de828ae7f30e1d2938f3d1703bd7ef
Purvanas <arnaudmirocha@gmail.com>
2023-03-17 12:28:13 +0100

    inventaire

---
+++ rpg-manager/src/components/PersonageTableauInventaire.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const PersonageTableauInventaire = (props) => {
+
+    const objetJSON = props.value;
+    console.log(objetJSON)
+
+    const elements = Object.keys(objetJSON).map((key) => (
+        <div id="objet" key={key}>
+            <h3 id="objetTxt">{objetJSON[key].libelle}</h3>
+            <h3 id="objetTxt">{objetJSON[key].quantite}</h3>
+            <h3 id="objetTxt">{objetJSON[key].poids * objetJSON[key].quantite}</h3>
+            <h3 id="objetTxt">{objetJSON[key].details}</h3>
+        </div>
+    ));
+
+    return (
+        <div id="tableauObjet">
+            <div id="objetPlaceHolder" key="placeHolder">
+                <h3 id="objetTxtPlaceHolder">Objet</h3>
+                <h3 id="objetTxtPlaceHolder">Quantité</h3>
+                <h3 id="objetTxtPlaceHolder">Poids</h3>
+                <h3 id="objetTxtPlaceHolder">Détails</h3>
+            </div>
+            {elements}
+        </div>
+    );
+};
+
+export default PersonageTableauInventaire;
